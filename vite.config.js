7bc28fcfd2b7b06a5577e9b5df309adf311e1bd9
Michael Kirkland <mak.kirkland@proton.me>
2025-07-22 19:54:05 +0200

    Reapply "feat: Replace editor with CodeMirror 6 and enable custom autocompletions"
    This reverts commit f138b3f3d2d869a3c46036358e26aaae64d18044.

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,16 @@ const host = process.env.TAURI_DEV_HOST;
 export default defineConfig(async () => ({
   plugins: [sveltekit()],
 
+  optimizeDeps: {
+      exclude: [
+	  'svelte-codemirror-editor',
+	  'codemirror',
+	  '@codemirror/lang-markdown',
+	  '@codemirror/view',
+          '@codemirror/autocomplete'
+      ]
+  },
+
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
   // 1. prevent Vite from obscuring rust errors
